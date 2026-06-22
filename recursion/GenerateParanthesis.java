149c6d2f37c2835cd7c8aa546732c8e162f4db18
ganthantm65 <tmganthan@gmail.com>
2026-06-22 11:08:49 +0530

    palindrome-partition

---
+++ recursion/GenerateParanthesis.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParanthesis {
+    public static void main(String[] args){
+        List<String> res= new ArrayList<>();
+        int n=3;
+        backtracking(0,0,res,new StringBuilder(),n);
+        System.out.print(res.toString());
+    }
+    static void backtracking(int open,int close,List<String> res,StringBuilder str,int n){
+        if(str.length()==2*n){
+            res.add(str.toString());
+            return;
+        }
+        if(open<n){
+            str.append('(');
+            backtracking(open+1,close,res,str,n);
+            str.deleteCharAt(str.length()-1);
+        }
+        if(close<open){
+            str.append(')');
+            backtracking(open,close+1,res,str,n);
+            str.deleteCharAt(str.length()-1);
+        }
+    }
+}
